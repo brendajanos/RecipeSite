f3143734c832fb8aa8c512874e73bf74e63198c0
brendajanos <brenda.janos@icloud.com>
2021-11-04 22:57:39 +0100

    Recipe search works, basic css added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import "./App.css";
 import "./key";
 import Axios, * as others from "axios";
+import { useState } from "react";
 
 function App() {
+  const [query, setquery] = useState("");
+
   const YOUR_APP_ID = "4da09b8d";
   const YOUR_APP_KEY = "23c7b2c1d347e5914627e18225d85a11";
 
-  var url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=alcohol-free`;
+  var url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=alcohol-free`;
 
   async function getRecipes() {
     var result = await Axios.get(url);
     console.log(result.data);
   }
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipes();
+  };
+
   return (
-    <div className="App">
+    <div className="app">
       <h1 onClick={getRecipes}>🍔Food Recipe Plaza🍔</h1>
+      <form className="app_searchForm" onSubmit={onSubmit}>
+        <input
+          type="text"
+          className="app__input"
+          placeholder="Enter Ingridient"
+          value={query}
+          onChange={(e) => setquery(e.target.value)}
+        />
+        <input className="app__submit" type="submit" value="Search" />
+      </form>
     </div>
   );
 }

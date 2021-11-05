53664f8056960c2319123cb0af840001d2cc31c3
brendajanos <brenda.janos@icloud.com>
2021-11-05 12:37:07 +0100

    Healthlabels added,CSS finalized

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import RecipeTile from "./RecipeTile";
 function App() {
   const [query, setquery] = useState("");
   const [recipes, setrecipes] = useState([]);
+  const [healthLabel, sethealthLabel] = useState("vegan");
 
   const YOUR_APP_ID = "4da09b8d";
   const YOUR_APP_KEY = "23c7b2c1d347e5914627e18225d85a11";
 
-  var url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=alcohol-free`;
+  var url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabel}`;
 
   async function getRecipes() {
     var result = await Axios.get(url);
@@ -36,10 +37,22 @@ function App() {
           onChange={(e) => setquery(e.target.value)}
         />
         <input className="app__submit" type="submit" value="Search" />
+
+        <select className="app__healthLabels">
+          <option onClick={() => sethealthLabel("vegan")}>Vegan</option>
+          <option onClick={() => sethealthLabel("paleo")}>Paleo</option>
+          <option onClick={() => sethealthLabel("kosher")}>Kosher</option>
+          <option onClick={() => sethealthLabel("low-sugar")}>Low-sugar</option>
+          <option onClick={() => sethealthLabel("keto-friendly")}>Keto-friendly</option>
+          <option onClick={() => sethealthLabel("dairy-free")}>Dairy-free</option>
+          <option onClick={() => sethealthLabel("gluten-free")}>Gluten-free</option>
+          <option onClick={() => sethealthLabel("peanut-free")}>Peanut-free</option>
+        </select>
       </form>
-      <div>
+
+      <div className="app__recipes">
         {recipes.map((recipe) => {
-          return <RecipeTile recipe={recipe} />
+          return <RecipeTile recipe={recipe} />;
         })}
       </div>
     </div>
--- src/RecipeTile.js
+++ src/RecipeTile.js
@@ -3,9 +3,11 @@ import "./RecipeTile.css";
 
 export default function RecipeTile({ recipe }) {
   return (
-    <div className="recipeTile">
-      <img className="recipeTile__img" src={recipe["recipe"]["image"]}/>
-      <p className="recipeTile__name">{recipe["recipe"]["label"]}</p>
-    </div>
+    recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
+      <div className="recipeTile">
+        <img className="recipeTile__img" src={recipe["recipe"]["image"]} />
+        <p className="recipeTile__name">{recipe["recipe"]["label"]}</p>
+      </div>
+    )
   );
 }
